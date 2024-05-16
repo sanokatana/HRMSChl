8c0ce60550ae2ac7127b0d77a72ffc5207e195e3
sanokatana <chandrazahran@gmail.com>
2024-05-16 22:26:59 +0700

    Update

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KaryawanController extends Controller
 {
     public function index(){
-        return view("karyawan.index");
+
+        $karyawan = DB::table('karyawan')
+        ->join('department','karyawan.kode_dept','=','department.kode_dept')
+        ->orderBy('nama_lengkap')
+        ->get();
+        return view("karyawan.index", compact('karyawan'));
     }
 }
--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -18,22 +18,51 @@
 </div>
 <div class="page-body">
     <div class="container-xl">
-    <div class="row">
-        <div class="col-12">
-            <table class="table table-boder">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>NIK</th>
-                        <th>Nama</th>
-                        <th>Jabatan</th>
-                        <th>Department</th>
-                        <th>No. Hp</th>
-                    </tr>
-                </thead>
-            </table>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-vcenter card-table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>NIK</th>
+                                    <th>Nama</th>
+                                    <th>Jabatan</th>
+                                    <th>No. Hp</th>
+                                    <th>Foto</th>
+                                    <th>Department</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($karyawan as $d)
+                                @php
+                                    $path = Storage::url('uploads/karyawan/'.$d->foto)
+                                @endphp
+                                <tr>
+                                    <td>{{ $loop->iteration}}</td>
+                                    <td>{{ $d->nik}}</td>
+                                    <td>{{ $d->nama_lengkap}}</td>
+                                    <td>{{ $d->jabatan}}</td>
+                                    <td>{{ $d->no_hp}}</td>
+                                    <td>
+                                        @if (empty($d->foto))
+                                        <img src="{{ asset('assets/img/nophoto.jpg')}}" class="avatar" alt="">
+                                        @else
+                                        <img src="{{ url($path )}}" class="avatar" alt="">
+                                        @endif
+                                    </td>
+                                    <td>{{ $d->nama_dept}}</td>
+                                    <td></td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 @endsection
